dcd1886262545291bf7138a48394099b47959a8b
Migacz85 <migac85@gmail.com>
2025-07-06 20:43:12 +0100

    changes

--- init.sh
+++ init.sh
@@ -13,9 +13,38 @@ apt install -y vim &&
 apt install -y xsel &&
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
-~/.fzf/install
+
+#add ctrl + r for history to shell
+BASHRC="$HOME/.bashrc"
+FZF_BINDING_FUNCTION="__fzf_history_search"
+FZF_BINDING_LINE='bind -x '"'"'\C-r": __fzf_history_search'"'"''
+
+# Check if the function is already in .bashrc
+if ! grep -q "$FZF_BINDING_FUNCTION" "$BASHRC"; then
+  echo "Adding fzf Ctrl+R binding to $BASHRC..."
+
+  cat << 'EOF' >> "$BASHRC"
+
+# fzf-based reverse history search
+__fzf_history_search() {
+  local selected=$(HISTTIMEFORMAT= history | fzf +s --tac | sed 's/ *[0-9]* *//')
+  if [ -n "$selected" ]; then
+    READLINE_LINE="$selected"
+    READLINE_POINT=${#READLINE_LINE}
+  fi
+}
+
+bind -x '"\C-r": __fzf_history_search'
+EOF
+
+  echo "Done. Please run: source ~/.bashrc"
+else
+  echo "fzf Ctrl+R binding already present in $BASHRC"
+fi
+
+
+
 source ~/.bashrc
 
-rm -R shell-config
 
 echo "All done, git, ranger, vim and fzf autocompletion is installed. Press ctrl+R to browse history"
